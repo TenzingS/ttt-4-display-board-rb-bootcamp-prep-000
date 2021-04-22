87b421e1647276f589d7ac5b83085f8204a19b36
Tenzing Sherpa <thesherpatenzing@gmail.com>
2021-04-22 20:35:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,11 @@
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 
 def display_board(board)
-  "#{board[4]}" = "X"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  board[4] = "X"
 end
